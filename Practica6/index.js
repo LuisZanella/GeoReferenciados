3c61ec84dcc21b845f5c4e70340fd9e49c2cd0ef
LuisZanella <luiscontreraszane@gmail.com>
2020-03-10 18:58:04 -0600

    Practice 6 fixed

--- Practica6/index.js
+++ Practica6/index.js
@@ -1,31 +1,31 @@
 var map;
 // var coordinates = new google.maps.LatLng(0, 0);
 var coordinates = {
-  lat: 0,
-  lng: 0
-};
-var props = {
-  center: coordinates,
+  center: { lat: 0, lng: 0 },
   zoom: 20
 };
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), props);
-  var icono = {
-    url: "",
+
+function initMap(params) {
+  map = new google.maps.Map(document.getElementById("map"), coordinates);
+  var icon = {
+    url:
+      "https://pngimage.net/wp-content/uploads/2018/06/we-are-here-png-2.png",
     scaledSize: new google.maps.Size(50, 50),
     origin: new google.maps.Point(0, 0),
     anchor: new google.maps.Point(0, 0)
   };
   var marker = new google.maps.Marker({
     position: { lat: 0, lng: 0 },
-    icono: icono,
+    icon,
     scaledSize: new google.maps.Size(50, 50),
-    map: map
+    map
   });
-  if (navigator.geolocation)
+  if (navigator.geolocation) {
+    movePosition(marker);
     setInterval(() => {
       movePosition(marker);
     }, 3000);
+  }
 }
 
 function movePosition(marker) {
