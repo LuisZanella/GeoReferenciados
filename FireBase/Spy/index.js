c03d428a16b95e0677cb8fd59193425e609aa831
LuisZanella <luiscontreraszane@gmail.com>
2020-03-20 21:14:16 -0600

    Practice spy register part fixed

--- FireBase/Spy/index.js
+++ FireBase/Spy/index.js
@@ -18,7 +18,7 @@ var firebaseConfig = {
 };
 let userName = "";
 const namesRegistered = [];
-var flag = false;
+var registerFlag = true;
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
@@ -79,20 +79,24 @@ function initMap() {
   }
 }
 addPosition = () => {
-  flag = namesRegistered
-    .map(data => {
-      return data === userName;
-    })
-    .includes(true);
-  console.log(flag);
-  if (!flag && userName !== undefined && userName !== "") {
-    navigator.geolocation.getCurrentPosition(position => {
-      db.collection("Spy").add({
-        Latitude: position.coords.latitude,
-        Longitude: position.coords.longitude,
-        Name: userName
+  if (registerFlag) {
+    var flag = false;
+    flag = namesRegistered
+      .map(data => {
+        return data === userName;
+      })
+      .includes(true);
+    console.log(flag);
+    if (!flag && userName !== undefined && userName !== "") {
+      register = false;
+      navigator.geolocation.getCurrentPosition(position => {
+        db.collection("Spy").add({
+          Latitude: position.coords.latitude,
+          Longitude: position.coords.longitude,
+          Name: userName
+        });
       });
-    });
+    }
   }
 };
 
