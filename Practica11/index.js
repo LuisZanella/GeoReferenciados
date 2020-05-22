883b0915869724593378b5f27b5d1008bf794708
LuisZanella <luiscontreraszane@gmail.com>
2020-05-22 11:20:57 -0500

    Map zoom changed practice 10

--- Practica11/index.js
+++ Practica11/index.js
@@ -296,7 +296,7 @@ function initMap() {
             lat: 21.152639,
             lng: -101.711598
         },
-        zoom: 50,
+        zoom: 100,
         mapTypeControlOptions: {
             mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'style_map']
         }
